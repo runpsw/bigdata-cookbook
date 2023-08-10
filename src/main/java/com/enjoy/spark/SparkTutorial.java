68fffc4603bfc4629fe930906cf4a8fcfefd83ba
psw <psw@cypc.com>
2023-08-11 00:02:37 +0800

    ## 添加了spark core api 读取文件的案例  - textFile()  - wholeTextFiles()

--- src/main/java/com/enjoy/spark/SparkTutorial.java
+++ src/main/java/com/enjoy/spark/SparkTutorial.java
@@ -4,10 +4,13 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.jetbrains.annotations.NotNull;
 import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author psw
@@ -19,12 +22,29 @@ public class SparkTutorial {
 
     public static void main(String[] args) {
 
-        SparkConf conf = new SparkConf()
-                .setAppName("spark core tutorial")
-                .setMaster("local");
+        JavaSparkContext sc = createDefualSc();
 
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        // word count simple
+//        wordCountSimple(sc);
 
+        // read from local file system
+        String path = "data/wc-data.txt";
+        String pairPath = "data/pair.txt";
+        JavaRDD<String> strRdd = sc.textFile(path);
+        JavaPairRDD<String, String> pairRdd = sc.wholeTextFiles(pairPath);
+
+//        strRdd.collect().stream().forEach(System.out::println);
+        System.out.println("--------------------------------");
+        Map<String, String> map = pairRdd.collectAsMap();
+        Set<String> strings = map.keySet();
+        for (String string : strings) {
+            System.out.println(string + "->" + map.get(string));
+        }
+
+        sc.close();
+    }
+
+    private static void wordCountSimple(JavaSparkContext sc) {
         String path = "data/wc-data.txt";
         JavaRDD<String> rowRdd = sc.textFile(path);
 
@@ -42,7 +62,16 @@ public class SparkTutorial {
         List<Tuple2<String, Integer>> collect = sortedByValue.mapToPair(tp -> new Tuple2<>(tp._2(), tp._1())).collect();
 
         System.out.println(collect);
+    }
 
-        sc.close();
+    @NotNull
+    private static JavaSparkContext createDefualSc() {
+        SparkConf conf = new SparkConf()
+                .setAppName("spark core tutorial")
+                .setMaster("local");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("WARN");
+        return sc;
     }
 }
